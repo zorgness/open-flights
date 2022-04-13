7769b928b59a57ae541301d656beadfeff6478d4
zorgness <tfouquert@gmail.com>
2022-04-13 13:29:40 +0200

    change ruby v

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
 require 'devise'
+require 'yaml'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
